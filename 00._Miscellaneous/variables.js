1029a084a67c97fc396d4432d79695b1256c3142
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-02-03 10:35:05 +0100

    Created the variables files

---
+++ 00._Miscellaneous/variables.js
@@ -0,0 +1,19 @@
+const me = {
+    name: "Zando"
+};
+me.name = "Mikkel";
+
+const hobbies = ["Football", "Swimming"];
+hobbies.push("Cooking");
+
+me.hobbies = hobbies;
+
+console.log(me)
+
+
+const hobbyOne = "Football skill level: '6'";
+const hobbyTwo = 'Swimming skill level: "3"';
+const hobbyThree = `Cooking skill level: "'${2+2}'"`;
+console.log(hobbyOne);
+console.log(hobbyTwo);
+console.log(hobbyThree);
