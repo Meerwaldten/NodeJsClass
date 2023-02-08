fb64bb1400cc21880f31a4e092c0ff95d2cb03fe
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-02-08 12:10:59 +0100

    added an answer for an exercise that I missed

--- 01._Exercises/Intro exercises/4.Arrays.js
+++ 01._Exercises/Intro exercises/4.Arrays.js
@@ -34,6 +34,11 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
 
+const find1729 = significantMathNumbers.indexOf(1729, 0);
+
+console.log(find1729);
+
+
 
 
 
