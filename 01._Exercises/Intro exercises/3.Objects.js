ea7512f44438733ce310afd16b38cc3554e5889f
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-02-08 12:05:41 +0100

    Trying to add answers for exercise 3 & 4

--- 01._Exercises/Intro exercises/3.Objects.js
+++ 01._Exercises/Intro exercises/3.Objects.js
@@ -7,18 +7,27 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObj);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const myNameAndAge = {age : 28, name : "Mikkel"};
+
+console.log(myNameAndAge);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
 
 const stackOverflow = {};
 
+stackOverflow.isAllowed = true;
+
 // make a rule called isAllowed and let the value be true
+
+console.log(stackOverflow);
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
@@ -27,6 +36,10 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
+
+console.log(thisSong);
 
 // --------------------------------------
 
--- 01._Exercises/Intro exercises/4.Arrays.js
+++ 01._Exercises/Intro exercises/4.Arrays.js
@@ -6,6 +6,10 @@
 const letters = ["a","b","c"];
 // show b in the console 
 
+const secondLetterInArray = letters[1];
+
+console.log(secondLetterInArray)
+
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -14,6 +18,13 @@ const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
+const friend1 = "Mark";
+const friend2 = "Peter";
+const friend3 = "Rasmus";
+
+friends.push(friend1, friend2, friend3);
+
+console.log(friends);
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -24,6 +35,8 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
 
 
+
+
 // --------------------------------------
 // Exercise 4 - Inserting elements
 
@@ -31,7 +44,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
+diet.splice(2, 0, "hamburger", "soda", "pizza");
 
+console.log(diet);
 
 
 // --------------------------------------
@@ -41,7 +56,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
 
-
+diet.pop();
+console.log(diet);
 
 
 // --------------------------------------
@@ -49,7 +65,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
+const dinnerTray = [...diet];
 
+console.log(dinnerTray);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -59,6 +77,12 @@ const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 // log every second char in the array starting from b
 
 
+for (let index = 0; index < lettersExpanded.length; index++) {
+    const element = lettersExpanded[index];
+    if(index % 2 == 1){
+        console.log(element)
+    }   
+}
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -66,6 +90,15 @@ const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 const numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
 
 const discardedNumbers = [];
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element > 6 || element < 0){
+        console.log(element);
+    }else {
+        discardedNumbers.push(element);
+    };
+}
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
