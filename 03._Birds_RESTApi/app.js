b35c4a0df1e4a971ab62487bf1b56c4781e930e8
Meerwaldten <mikkell_m_j@hotmail.com>
2023-02-16 18:31:11 +0100

    Added multiple bird objects with attributes to the birds array.

--- 03._Birds_RESTApi/app.js
+++ 03._Birds_RESTApi/app.js
@@ -8,14 +8,19 @@ app.get("/", (req, res) => {
 });
 
 
-let birds = ["Sparrow", "Eagle", "Falcon"]
+const birds = []
+const bird1 ={id: 1, name: "Sparrow"};
+const bird2 ={id: 2, name: "Eagle"};
+const bird3 ={id: 3, name: "Falcon"};
+birds.push(bird1, bird2, bird3);
+
+
 app.get("/birds", (req, res) => {
     res.send({ 
         message: 
-        `
-        First bird is: ${birds[0]} 
-        Second bird is: ${birds[1]}
-        Third bird is: ${birds[2]}`
+        `First bird is: ${birds[0].name} 
+        Second bird is: ${birds[1].name}
+        Third bird is: ${birds[2].name}`
     });
 });
 
@@ -23,19 +28,9 @@ app.get("/birds/:id", (req, res) => {
     let id = req.params['id'];
     res.send({
         message: 
-        `You have chosen: ${birds[id]}`
+        `You have chosen: ${birds[id].name}`
     })
 })
-
-
-
-
-
-
-
-
-
-
 
 
 
