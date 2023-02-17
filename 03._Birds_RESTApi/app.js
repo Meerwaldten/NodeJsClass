94a222a68c2d203a22d44428a7d36dff012cf829
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-02-17 09:06:34 +0100

    Added new solutions to get birds/:id

--- 03._Birds_RESTApi/app.js
+++ 03._Birds_RESTApi/app.js
@@ -17,20 +17,20 @@ birds.push(bird1, bird2, bird3);
 
 
 app.get("/birds", (req, res) => {
-    res.send({ 
-        message: 
-        `First bird is: ${birds[0].name} 
-        Second bird is: ${birds[1].name}
-        Third bird is: ${birds[2].name}`
-    });
+    res.send({ data: birds});
 });
 
 app.get("/birds/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const birdById = birds.find((bird) => bird.id === id);
-    res.json(birdById)
+    //const id = Number(req.params.id); 
+    //const bird = birds.find((bird) => bird.id === id);
+    //res.json(bird);
+
+    const bird = birds.find((bird) => bird.id === Number(req.params.id));
+    res.send({data: bird});
 });
 
 
 
-app.listen(8080);
+app.listen(8080, () => {
+    console.log("Server is running ong port", 8080)
+});
