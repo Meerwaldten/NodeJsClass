7e54b2fc3dfd82966ed721b0200248e9a698e1be
Meerwaldten <mikkell_m_j@hotmail.com>
2023-02-22 14:34:24 +0100

    Added rest of CRUD methods for birds API

--- 03._Birds_RESTApi/app.js
+++ 03._Birds_RESTApi/app.js
@@ -9,11 +9,15 @@ app.get("/", (req, res) => {
 });
 
 
+const generateId = (() => (id = 0, () => id++))();
+
 const birds = []
-const bird1 ={id: 1, name: "Sparrow"};
-const bird2 ={id: 2, name: "Eagle"};
-const bird3 ={id: 3, name: "Falcon"};
+const bird1 ={id: generateId(), name: "Sparrow"};
+const bird2 ={id: generateId(), name: "Eagle"};
+const bird3 ={id: generateId(), name: "Falcon"};
 birds.push(bird1, bird2, bird3);
+
+
 
 
 app.get("/birds", (req, res) => {
@@ -21,31 +25,38 @@ app.get("/birds", (req, res) => {
 });
 
 app.get("/birds/:id", (req, res) => {
-    //const id = Number(req.params.id); 
-    //const bird = birds.find((bird) => bird.id === id);
-    //res.json(bird);
     const bird = birds.find((bird) => bird.id === Number(req.params.id));
     res.send({data: bird});
 });
 
 
 app.post("/birds", (req, res) => {
-
+    const birdName = req.body.name;
+    const newBird = {id: generateId(), name: birdName};
+    birds.push(newBird);
+    res.send({ data: newBird});
 })
 
 app.put("/birds/:id", (req, res) => {
-
+    const bird = birds.find((bird) => bird.id ===Number(req.params.id));
+    const newBirdName = req.body.name;
+    bird.name = newBirdName;
+    res.json(bird);
 })
 
 
 app.patch("/birds/:id", (req, res) => {
-
+    const bird = birds.find((bird) => bird.id ===Number(req.params.id));
+    const newBirdName = req.body.name;
+    bird.name = newBirdName;
+    res.json(bird);
 })
 
 app.delete("/birds/:id", (req, res) => {
-
+    const idToRemove = req.params.id;
+    birds.splice(idToRemove, 1);
+    res.json(birds);
 })
-
 
 
 
