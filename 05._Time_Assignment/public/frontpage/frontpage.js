9de5616f064a10e1390feaba29776a09c1e21753
Meerwaldten <mikkell_m_j@hotmail.com>
2023-02-27 13:08:17 +0100

    Vercel test 10

--- 05._Time_Assignment/public/frontpage/frontpage.js
+++ 05._Time_Assignment/public/frontpage/frontpage.js
@@ -1,9 +1,3 @@
-/*
-There are some obvious flaws in the design. Including what happens when we pass the appointed date.
-I've also just reused the original date for the daily timer, with having removed days from it since the time of day is the same.
-Excuse the bad design on my flu-hit brain over the weekends.
-*/
-
 const weeklyCountDownDate = new Date("Mar 1, 2023 04:00:00").getTime();
 
 var myfunc = setInterval(function() {}, 1000)
@@ -19,5 +13,3 @@ var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
 document.getElementById("weeklytimer").innerText = "The next weekly reset is in: " + days + " days, " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds.";
 document.getElementById("dailytimer").innerText = "The next daily reset is in: " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds.";
-
-
