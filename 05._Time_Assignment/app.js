fa38720d9d5a7b6fca4e766f0a0c0fc71a8385c9
Meerwaldten <mikkell_m_j@hotmail.com>
2023-02-27 12:42:36 +0100

    More vercel test 1

--- 05._Time_Assignment/app.js
+++ 05._Time_Assignment/app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 });
 
 
-
+/*
 const PORT = 8080;
 app.listen(PORT, (error) => {
     if(error) {
@@ -18,3 +18,4 @@ app.listen(PORT, (error) => {
     }
     console.log("Server is running on port:", PORT);
 });
+*/
